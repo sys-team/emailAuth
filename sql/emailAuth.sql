f0328b6b434612f234f71170dfc35c45261c9ed0
Alexander Levin <alevin.ru@gmail.com>
2014-09-13 09:02:31 +0300

    ea.emailAuth exception handler

--- sql/emailAuth.sql
+++ sql/emailAuth.sql
@@ -1,30 +1,45 @@
 create or replace function ea.emailAuth(@url long varchar)
 returns xml
 begin
+
     declare @response xml;
 
-    set @response = case @url
-        when 'login' then
-            ea."login"()
-        when 'register' then
-            ea.register()
-        when 'confirm' then
-            ea.confirm()
-        when 'roles' then
-            ea.roles()
-        when 'check' then
-            ea."check"()
-        when 'token' then
-            ea.token()
-        when 'confirmInvite' then
-            ea.confirmInvite()
-        when 'invite' then
-            ea.invite()
-        else
-            'Unknown service request'
-    end case;
+    begin
 
-    set @response = xmlelement('response', xmlattributes('https://github.com/sys-team/UOAuth' as "xmlns"), @response);
+        set @response = case @url
+            when 'login' then
+                ea."login"()
+            when 'register' then
+                ea.register()
+            when 'confirm' then
+                ea.confirm()
+            when 'roles' then
+                ea.roles()
+            when 'check' then
+                ea."check"()
+            when 'token' then
+                ea.token()
+            when 'confirmInvite' then
+                ea.confirmInvite()
+            when 'invite' then
+                ea.invite()
+            else
+                'Unknown service request'
+        end;
+
+    exception when others then
+
+        set @response = xmlelement(
+            'error', xmlattributes('UnknownError' as "code"), errormsg()
+        );
+
+    end;
+
+    set @response = xmlelement(
+        'response',
+        xmlattributes('https://github.com/sys-team/UOAuth' as "xmlns"),
+        @response
+    );
 
     return @response;
 
