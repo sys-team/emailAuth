26dea1dd9ff72917a934764f28c37f36bf8e7e38
DeadCat93 <alexey.i.panshin@gmail.com>
2015-03-12 16:43:11 +0300

    Bug fixed

--- sql/emailAuth.sql
+++ sql/emailAuth.sql
@@ -3,8 +3,9 @@ create or replace function ea.emailAuth(
 )
 returns xml
 begin
-
     declare @response xml;
+    declare @error STRING;
+    declare @sqlstate STRING;
 
     begin
 
@@ -36,9 +37,16 @@ begin
         end;
 
     exception when others then
-
+    
+        set @error = errormsg();
+        set @sqlstate = SQLSTATE;
+        
+        rollback;       
+        
+        call util.errorHandler('ea.emailAuth', @sqlstate, @error);
+            
         set @response = xmlelement(
-            'error', xmlattributes('UnknownError' as "code"), errormsg()
+            'error', xmlattributes('UnknownError' as "code"), @error
         );
 
     end;
