ccc3a5d4b31548e6dd20331373115d7c284759ee
DeadCat93 <alexey.i.panshin@gmail.com>
2015-04-06 11:20:21 +0300

    util.newMail in sendConfirmation

--- sql/sendConfirmation.sql
+++ sql/sendConfirmation.sql
@@ -6,19 +6,19 @@ create or replace procedure ea.sendConfirmation(
     @subject long varchar default util.getUserOption('ea.confirmationSubject')
 )
 begin
-
+    declare @id integer;
     declare @email long varchar;
     declare @code long varchar;
     declare @msg long varchar;
     declare @addChar varchar(24);
-    
+
     set @addChar = if locate(@callback,'?') <> 0 then '&' else '?' endif;
-    
+
     select email,
            confirmationCode
       into @email, @code
       from ea.account where id = @userId;
-      
+
     if util.getUserOption('ea.confirmationMessage') is null then
 
         set @msg = xmlconcat(
@@ -30,26 +30,26 @@ begin
             else
                 null
             endif);
-    
+
     else
-    
+
         set @msg = replace(replace(util.getUserOption('ea.confirmationMessage'), '@code', @code), '@callback', @callback + @addChar + 'code=' + @code);
-        
+
     end if;
 
     set @subject = isnull(@subject, @smtpSender + ' confirmation');
-    
+
     if isnull(util.getUserOption('ea.emailHtml'), '0') = '1' then
-    
-        call util.emailHtml(
-            @bodyHtml = @msg,
-            @subject = @subject,
-            @smtp_server = isnull(util.getUserOption('SMTPServer'), 'localhost'),
-            @smtp_sender = isnull(util.getUserOption('SMTPSender'), 'emailAuth'),
-            @smtp_sender_name = isnull(util.getUserOption('SMTPSenderName'), 'emailAuth'),
-            @recipient = @email
+
+        set @id = util.newMail(
+            @email,
+            @subject,
+            @msg,
+            util.getUserOption('utilMail.defaultAccount'),
+            isnull(util.getUserOption('SMTPSender'), 'emailAuth'),
+            isnull(util.getUserOption('SMTPSenderName'), 'emailAuth')
         );
-    
+
     else
         if @smtpSender is null then
             call util.email(@email, @subject, @msg);
